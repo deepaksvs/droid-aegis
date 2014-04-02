a59a3ea7bf14e48b86bedb6171ba376fc36850f7
naane <naane@naanu.com>
2014-04-02 22:03:14 +0530

    New app for Froyo with previewcallback

---
+++ CamStreamer/src/com/app/camstreamer/MainActivity.java
@@ -0,0 +1,137 @@
+package com.app.camstreamer;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+
+public class MainActivity extends Activity {
+	private static final String tag = "CameraMain";
+	private SurfaceView		mSurfaceView;
+	private MockCamera		mCamera;
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        if (savedInstanceState == null) {
+//            getSupportFragmentManager().beginTransaction()
+//                    .add(R.id.container, new PlaceholderFragment())
+//                    .commit();
+//        }
+        
+        Log.d(tag, "onCreate of Camera MainActivity");
+        mCamera		= new MockCamera();
+        mSurfaceView = new MockSurfaceView (this);
+        setContentView(mSurfaceView);
+    }
+
+    @Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		Log.d(tag,"OnPause for MainActivity");
+		mCamera.release();
+		super.onPause();
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		// TODO Auto-generated method stub
+		Log.d(tag,"OnDestroy for MainActivity");
+		mCamera = null;
+	}
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+    }
+
+    private class MockSurfaceView extends SurfaceView implements SurfaceHolder.Callback {
+    	private SurfaceHolder	mHolder;
+    	private static final int WIDTH_CONST = 320;
+    	private static final int HEIGHT_CONST = 240;
+
+		@SuppressWarnings("deprecation")
+		public MockSurfaceView(Context context) {
+			super(context);
+			// TODO Auto-generated constructor stub
+			mHolder = getHolder();
+			mHolder.addCallback(this);
+			mHolder.setFixedSize(WIDTH_CONST, HEIGHT_CONST);
+			mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+		}
+
+		@Override
+		public void surfaceChanged(SurfaceHolder holder, int format, int width,
+				int height) {
+			// TODO Auto-generated method stub
+			Log.d(tag, "on SurfaceChanged");
+			Log.d(tag, "width = " + width + " height = " + height);
+			mCamera.setMockPreviewSize(width, height);
+			Thread mt = new Thread() {
+				public void run () {
+					mCamera.initCamera();
+					mCamera.doPreview(mSurfaceView.getHolder());
+				}
+			};
+			mt.start();
+		}
+
+		@Override
+		public void surfaceCreated(SurfaceHolder holder) {
+			// TODO Auto-generated method stub
+			Log.d(tag, "on SurfaceCreated");
+		}
+
+		@Override
+		public void surfaceDestroyed(SurfaceHolder holder) {
+			// TODO Auto-generated method stub
+			Log.d(tag, "on SurfaceDestroyed");
+		}
+    	
+    }
+}
---
+++ CamStreamer/src/com/app/camstreamer/MockCamera.java
@@ -0,0 +1,102 @@
+package com.app.camstreamer;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import android.graphics.ImageFormat;
+import android.graphics.PixelFormat;
+import android.hardware.Camera;
+import android.hardware.Camera.ErrorCallback;
+import android.hardware.Camera.PictureCallback;
+import android.hardware.Camera.PreviewCallback;
+import android.hardware.Camera.Size;
+import android.util.Log;
+import android.view.SurfaceHolder;
+
+public class MockCamera {
+	private static final String tag = "MockCamera";
+	private Camera		mCam;
+	private int			mWidth, mHeight;
+	private CameraErrors	mCamErr;
+	private PreviewHandler	mPrevhandler;
+	
+	public MockCamera() {
+		// TODO Auto-generated constructor stub
+		mCam = null;
+		mWidth = mHeight = 0;
+		mCamErr = new CameraErrors();
+		mPrevhandler = new PreviewHandler();
+	}
+	
+	public boolean initCamera () {
+		mCam = Camera.open();
+		if (mCam == null) {
+			Log.d(tag, "Failed to open Camera");
+			return false;
+		}
+		mCam.setErrorCallback(mCamErr);
+		return true;
+	}
+	
+	public void setMockPreviewSize (int width, int height) {
+		mWidth = width;
+		mHeight = height;
+	}
+	
+	@SuppressWarnings("deprecation")
+	public void doPreview (SurfaceHolder mHolder) {
+		try {
+			mCam.setPreviewDisplay(mHolder);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Camera.Parameters	mParams = mCam.getParameters();
+//		Log.d(tag, "Picture format " + mParams.getPictureFormat());
+//		Size = mParams.getPreviewSize();
+
+		mParams.setPictureSize(mWidth, mHeight);
+//		mParams.setPictureFormat(ImageFormat.NV16);
+//		List<Integer> pitformat = mParams.getSupportedPictureFormats();
+//		List<Integer> prvFormat = mParams.getSupportedPreviewFormats();
+//		Log.d(tag, "Picture format " + Arrays.toString(picformat.toArray()));
+//		Log.d(tag, "items = " + prvFormat.size() + " Preview format " + Arrays.toString(prvFormat.toArray()));
+		mParams.setPreviewFrameRate(15);
+		mCam.setParameters(mParams);
+		mParams = mCam.getParameters();
+//		mCam.setPreviewCallback(mPrevhandler);
+		mCam.startPreview();
+		Size sz = mParams.getPreviewSize();
+//		Log.d(tag, "Preview Size w = " + sz.height + " h = " + sz.height);
+//		Log.d(tag, "BytesPerPixel = " + ImageFormat.getBitsPerPixel(mParams.getPreviewFormat()));
+//		Log.d(tag, "Preview format " + mParams.getPreviewFormat());
+//		Log.d(tag, "Picture frame rate " + mParams.getPreviewFrameRate());
+	}
+	
+	public void release () {
+		mCam.setPreviewCallback(null);
+		mCam.stopPreview();
+		mCam.release();
+	}
+	
+	private class PreviewHandler implements PreviewCallback {
+
+		@Override
+		public void onPreviewFrame(byte[] data, Camera camera) {
+			// TODO Auto-generated method stub
+			Log.d(tag, "PreviewFrame received");
+		}
+		
+	}
+	
+	private class CameraErrors implements ErrorCallback {
+
+		@Override
+		public void onError(int error, Camera camera) {
+			// TODO Auto-generated method stub
+			Log.d(tag, "Camera onError " + error);
+		}
+		
+	}
+}
